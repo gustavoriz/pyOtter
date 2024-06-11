53e008b27a2d78d0fbd4ea7632fa6aa0901c504e
gustavoriz <73759398+gustavoriz@users.noreply.github.com>
2024-06-11 18:51:56 -0300

    Update pyOtter.py

--- pyOtter.py
+++ pyOtter.py
@@ -1,4 +1,6 @@
-# Criado por: Gustavo Riz
+# This library was created to convert spark dataframes or pandas dataframes to otter dataframes
+# Created by: Gustavo Riz
+# Version: 1.1
 class pyOtter:
     def __init__(self, data=None, columns=None):
         self.data = data if data is not None else []
@@ -9,10 +11,55 @@ class pyOtter:
         columns = spark_df.columns
         data = [row.asDict() for row in spark_df.collect()]
         return cls(data, columns)
+    
+    def count_rows(self):
+        return len(self.data)
 
-    def filter(self, condition):
-        filtered_data = [row for row in self.data if condition(row)]
-        return MeuDataFrame(filtered_data, self.columns)
+    def rename_columns(self, col_rename_map):
+        self.columns = [col_rename_map.get(col, col) for col in self.columns]
+        for row in self.data:
+            for old_col, new_col in col_rename_map.items():
+                if old_col in row:
+                    row[new_col] = row.pop(old_col)
+    
+    def show_schema(self):
+        return {col: type(next(iter(self.data))[col]) for col in self.columns} if self.data else {}
+
+    def change_schema(self, col_type_map):
+        for row in self.data:
+            for col, new_type in col_type_map.items():
+                if col in row:
+                    row[col] = new_type(row[col])
+
+    def filter(self, **conditions):
+        filtered_data = []
+        for row in self.data:
+            include_row = True
+            for col, value in conditions.items():
+                if row.get(col) != value:
+                    include_row = False
+                    break
+            if include_row:
+                filtered_data.append(row)
+        return pyOtter(filtered_data, self.columns)
+
+    def count_distinct(self, *cols):
+        distinct_values = {col: set() for col in cols}
+        for row in self.data:
+            for col in cols:
+                distinct_values[col].add(row[col])
+        return {col: len(distinct_values[col]) for col in cols}
+
+    def export_to_csv(self, filename, delimiter=','):
+        with open(filename, 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=self.columns, delimiter=delimiter)
+            writer.writeheader()
+            writer.writerows(self.data)
+
+    def print_schema(self):
+        schema = self.show_schema()
+        for col, dtype in schema.items():
+            print(f"{col}: {dtype}")
 
     def join(self, other, on, how='inner'):
         joined_data = []
@@ -61,9 +108,9 @@ class pyOtter:
 
         # Resolve column conflicts
         joined_columns = list(set(self.columns + other.columns))
-        return MeuDataFrame(joined_data, joined_columns)
+        return pyOtter(joined_data, joined_columns)
 
     def __str__(self):
         header = "\t".join(self.columns)
-        rows = "\n".join("\t".join(str(row[col]) for col in self.columns) for row in self.data)
+        rows = "\n".join("\t".join(str(row.get(col, '')) for col in self.columns) for row in self.data)
         return f"{header}\n{rows}"
